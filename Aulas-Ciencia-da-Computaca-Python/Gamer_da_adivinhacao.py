32a67e28209725c71f7a62bb05c81e210046e8fb
MateusFerraz <mateus.p.ferraz@gmail.com>
2023-09-15 17:51:13 -0300

    Complementando o jogo
    Foi adicionado outras possibilidade de acertos e incluído uma espécie de pontuação

--- Aulas-Ciencia-da-Computaca-Python/Gamer_da_adivinhacao.py
+++ Aulas-Ciencia-da-Computaca-Python/Gamer_da_adivinhacao.py
@@ -6,10 +6,20 @@ print("Bem-vindo ao jogo!")
 entrada = input("Digite um número entre O e 100:")
 
 valor = int(entrada)
-if valor == 10:
-    print("Você  venceu.Parabéns!")
-else:
-    print("Vocé perdeu.Tente novamente!")
-
+#Aprimotamento do código
 print()
-print("G A M E   O V E R")
+
+if valor==10 or valor==20 or valor==30:
+    print("Você  venceu.Parabéns!")
+    print()
+    if valor ==10:
+        print ("Você ganhou 5 pontos")
+        print()
+    elif valor==20:
+        print("Você ganhou 100 pontos")
+    else:
+        print("Você ganhou 1000 pontos")
+else :
+        print("Você perdeu. Tente Novamente !")
+        print()
+        print("G A M E   O V E R")
